b5102b0273d07d30123ba475c2e965362ce82305
Anthony Okwananke <826717@ACDM.DS.SAIT.CA>
2020-07-13 15:37:48 -0600

    A react-redux app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import logo from './logo.svg';
 import './App.css';
+import CakeBox from './components/CakeBox';
+import { buyCake } from './redux/cake/cakeactions';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <CakeBox />
+      </div>
+    </Provider>
+
   );
 }
+
+// const mapStateToProps = state => {
+//   return {
+//     numOfCakes: state.numOfCakes
+//   }
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     buyCake: () => dispatch(buyCake())
+//   }
+// }
+
 
 export default App;
